48d58b7c52ed6a805844c11fa32fcc66811d8cf9
SlothDaemon <salomons.daniel@gmail.com>
2021-03-15 20:14:17 +0100

    Changed default behaviour to collapse article headings. Also removed 'WHATWG' from nav bar

--- js/script.js
+++ js/script.js
@@ -1,16 +1,30 @@
 function addHandlers(){
    const main = document.getElementsByTagName("main")[0]
    main.addEventListener("click", collapsibleHeaders);
+   collapseArticleHeaders();
 }
 
 function collapsibleHeaders(e) {
    if(e.target && e.target.nodeName == "H2"){
-      var c = e.target.nextElementSibling;
+      const c = e.target.nextElementSibling;
       e.target.classList.toggle("collapsed");
       if (c.style.display === "none") {
          c.style.display = "block";
       } else {
          c.style.display = "none";
+      }
+   }
+}
+
+function collapseArticleHeaders() {
+   // Default behaviour to have article headers collapsed, to 
+   // 1. Make articles less walls of text and 
+   // 2. To provide a contrast with aside headers which aren't collapsed, to teach visitors that headers can be (un)collapsed
+   const hs = document.getElementsByTagName('h2');
+   for (i=0; i<hs.length; i++) {
+      if (hs[i].parentElement.nodeName !== "ASIDE") {
+         hs[i].classList.toggle("collapsed");
+         hs[i].nextElementSibling.style.display="none";
       }
    }
 }
